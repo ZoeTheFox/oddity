1e73683c820225018c1cf0efdc154efd1069b819
Zoe <cmdrprotogen27@gmail.com>
2023-09-18 19:21:17 +0200

    added simple camera controls

---
+++ Oddity/Scenes/Spaceship/Scripts/CameraScripts/CameraController.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+@export
+var mouse_sensivity := 0.001
+
+var twist_input := 0.0
+var pitch_input := 0.0
+
+@onready
+var twist_pivot := $TwistPivot
+@onready
+var pitch_pivot := $TwistPivot/PitchPivot
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.is_action_pressed("camera-look-around"):
+		twist_pivot.rotate_y(twist_input)
+		pitch_pivot.rotate_x(pitch_input)
+		
+	twist_input = 0
+	pitch_input = 0
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensivity
+			pitch_input = - event.relative.y * mouse_sensivity
+			
+			print(twist_input)
