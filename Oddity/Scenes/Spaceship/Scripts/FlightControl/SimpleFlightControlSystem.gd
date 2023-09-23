f1402e1f536d7573fd36dc313969120afb7fb7fb
Zoe <cmdrprotogen27@gmail.com>
2023-09-23 17:34:04 +0200

    adding simple flight controls

--- Oddity/Scenes/Spaceship/Scripts/FlightControl/SimpleFlightControlSystem.gd
+++ Oddity/Scenes/Spaceship/Scripts/FlightControl/SimpleFlightControlSystem.gd
@@ -1,9 +1,22 @@
-extends Node3D
+extends RigidBody3D
 
 @export
-var spaceship_rigid_body := RigidBody3D
+var max_thrust_main: float
 
+@export
+var max_thrust_retro: float
 
+@export
+var max_thrust_up: float
+
+@export
+var max_thrust_down: float
+
+@export
+var max_thrust_left: float
+
+@export
+var max_thrust_right: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,4 +27,24 @@ func _ready():
 func _process(delta):
 	pass
 	
+func _on_user_control_thrust_forwards(throttle):
+	apply_central_force(Vector3(0,0,-max_thrust_main * (throttle / 100.0)) * get_process_delta_time()) # Replace with function body.
 
+func _on_user_control_thrust_backwards(throttle):
+	apply_central_force(Vector3(0,0,-max_thrust_retro * (throttle / 100.0)) * get_process_delta_time()) # Replace with function body.
+
+
+func _on_user_control_thrust_down(throttle):
+	apply_central_force(Vector3(0,-max_thrust_down * (throttle / 100.0),0 ) * get_process_delta_time()) # Replace with function body.
+
+
+func _on_user_control_thrust_left(throttle):
+	apply_central_force(Vector3(-max_thrust_left * (throttle / 100.0),0,0 ) * get_process_delta_time()) # Replace with function body.
+	
+
+func _on_user_control_thrust_right(throttle):
+	apply_central_force(Vector3(max_thrust_right * (throttle / 100.0),0,0 ) * get_process_delta_time()) # Replace with function body.
+
+
+func _on_user_control_thrust_up(throttle):
+	apply_central_force(Vector3(0, max_thrust_up * (throttle / 100.0), 0) * get_process_delta_time())
