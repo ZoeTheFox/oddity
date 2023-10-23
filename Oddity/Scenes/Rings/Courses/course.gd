1a58da3ae6405da2b2352c6c1a8421e2411cd45e
Zoe <cmdrprotogen27@gmail.com>
2023-10-23 09:17:30 +0200

    added cool ring effects

--- Oddity/Scenes/Rings/Courses/course.gd
+++ Oddity/Scenes/Rings/Courses/course.gd
@@ -5,7 +5,7 @@ extends Node3D
 var RingMarker : Node3D
 
 var course_rings = Array()
-var i = 0
+var rings_flown_through = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,10 +15,21 @@ func _ready():
 
 
 func ship_flew_through_ring():
-
-	i += 1
-	print(course_rings[i])
-	RingMarker.global_position = course_rings[i].global_position
+	if (rings_flown_through == 0):
+		RingMarker.show()
+	
+	if (rings_flown_through == (course_rings.size() - 1)):
+		print("you won")
+		
+		rings_flown_through = 0
+		RingMarker.hide()
+		
+		return 0;
+		
+	rings_flown_through += 1
+	
+	print(course_rings[rings_flown_through])
+	RingMarker.global_position = course_rings[rings_flown_through].global_position
 	
 
 	
