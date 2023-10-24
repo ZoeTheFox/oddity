ef099a85815b75dcb16c25c1b6d1dd5e9a1de66a
Zoe <cmdrprotogen27@gmail.com>
2023-10-24 12:31:24 +0200

    testing out camera shake

---
+++ Oddity/Scenes/Spaceship/Scripts/CameraScripts/CameraShake.gd
@@ -0,0 +1,17 @@
+extends Camera3D
+
+var acceleration : Vector3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+
+func _on_fighter_gen_7_accelleration_signal(accelaration):
+	self.acceleration = accelaration
