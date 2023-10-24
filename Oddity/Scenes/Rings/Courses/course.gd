e97fb67e70b3551dbd49bb77342bee35f17d1b4b
Zoe <cmdrprotogen27@gmail.com>
2023-10-24 08:53:45 +0200

    removed prints

--- Oddity/Scenes/Rings/Courses/course.gd
+++ Oddity/Scenes/Rings/Courses/course.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-
 @export
 var RingMarker : Node3D
 
@@ -10,7 +9,6 @@ var rings_flown_through = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for _i in $Rings.get_children ():
-		print(_i)
 		course_rings.append(_i)
 
 
@@ -19,7 +17,6 @@ func ship_flew_through_ring():
 		RingMarker.show()
 	
 	if (rings_flown_through == (course_rings.size() - 1)):
-		print("you won")
 		
 		rings_flown_through = 0
 		RingMarker.hide()
@@ -28,7 +25,6 @@ func ship_flew_through_ring():
 		
 	rings_flown_through += 1
 	
-	print(course_rings[rings_flown_through])
 	RingMarker.global_position = course_rings[rings_flown_through].global_position
 	
 
