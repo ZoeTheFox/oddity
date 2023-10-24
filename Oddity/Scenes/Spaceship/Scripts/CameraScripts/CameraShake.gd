a372c486d5546b86052193e7935d59cd01528586
Zoe <cmdrprotogen27@gmail.com>
2023-10-24 21:28:41 +0200

    added fov change based off speed

--- Oddity/Scenes/Spaceship/Scripts/CameraScripts/CameraShake.gd
+++ Oddity/Scenes/Spaceship/Scripts/CameraScripts/CameraShake.gd
@@ -1,6 +1,7 @@
 extends Camera3D
 
 var acceleration : Vector3
+var speed : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,9 +10,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	fov = adjustFOVBasedOnSpeed(speed)
 	
+	
+func adjustFOVBasedOnSpeed(speed: float) -> float:
+	# Define the reasonable FOV range
+	var min_fov = 60.0  # Minimum FOV
+	var max_fov = 90.0  # Maximum FOV
+
+	# Define the minimum and maximum speeds for mapping
+	var min_speed = -400.0
+	var max_speed = 400.0
+
+	# Ensure that the speed is within the specified range
+	speed = clamp(speed, min_speed, max_speed)
+
+	# Calculate the FOV based on speed using linear mapping
+	var fov = lerp(min_fov, max_fov, (speed - min_speed) / (max_speed - min_speed))
+
+	return fov
 
 
 func _on_fighter_gen_7_accelleration_signal(accelaration):
 	self.acceleration = accelaration
+
+
+func _on_fighter_gen_7_speed_signal(speed):
+	self.speed = speed
