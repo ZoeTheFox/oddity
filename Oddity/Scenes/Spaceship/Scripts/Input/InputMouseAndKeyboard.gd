b34f1cd499224b522d52dbbbafcb95f762458bc6
Zoe <cmdrprotogen27@gmail.com>
2023-11-18 18:01:04 +0100

    removed ready function

--- Oddity/Scenes/Spaceship/Scripts/Input/InputMouseAndKeyboard.gd
+++ Oddity/Scenes/Spaceship/Scripts/Input/InputMouseAndKeyboard.gd
@@ -25,11 +25,6 @@ var rotation_vector : Vector3
 signal send_movement_vector(movement_vector)
 signal send_rotation_vector(rotation_vector)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	movement_vector.x = 0
