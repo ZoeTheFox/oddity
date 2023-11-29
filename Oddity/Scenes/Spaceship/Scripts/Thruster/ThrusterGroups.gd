7b71a3989e12df38ce48936bf5d47aa1c72dc118
Zoe <cmdrprotogen27@gmail.com>
2023-11-29 14:52:53 +0100

    working on thruster

--- Oddity/Scenes/Spaceship/Scripts/Thruster/ThrusterGroups.gd
+++ Oddity/Scenes/Spaceship/Scripts/Thruster/ThrusterGroups.gd
@@ -7,13 +7,70 @@ extends Node3D
 @export
 var main_thrusters : Array
 
+@export_group("Retro")
 
+@export
+var retro_thrusters : Array
 
+@export_group("Left")
+
+@export
+var left_thrusters : Array
+
+@export_group("Right")
+
+@export
+var right_thrusters : Array
+
+@export_group("Top")
+
+@export
+var top_thrusters : Array
+
+@export_group("Bottom")
+
+@export
+var bottom_thrusters : Array
+
+@export_group("Roll-Right")
+
+@export
+var roll_right_thrusters : Array
+
+@export_group("Roll-Left")
+
+@export
+var roll_left_thrusters : Array
+
+@export_group("Pitch-Up")
+
+@export
+var pitch_up_thrusters : Array
+
+@export_group("Pitch-Down")
+
+@export
+var pitch_down_thrusters : Array
+
+@export_group("Yaw-Right")
+
+@export
+var yaw_right_thrusters : Array
+
+@export_group("Yaw-Left")
+
+@export
+var yaw_left_thrusters : Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for thruster in main_thrusters:
-		thruster.placeholder()
+		print(get_node(thruster).thruster_force)
+		get_node(thruster).thruster_force = 20
+	for thruster in main_thrusters:
+		print(get_node(thruster).thruster_force)
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
