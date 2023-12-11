1234f2105b5ae0e886dc9474b35f7243ae36e6c4
Zoe <cmdrprotogen27@gmail.com>
2023-12-11 14:08:14 +0100

    created fuel tank

---
+++ Oddity/Scenes/ShipComponents/Common/Fuel Tank/FuelTank.gd
@@ -0,0 +1,38 @@
+extends Node3D
+
+@export_category("Manufacturer Information")
+
+@export
+var manufacturer : String
+
+@export
+var model : String
+
+@export_category("Component Information")
+
+@export_range(0, 10, 1)
+var size_class : int
+
+@export
+var fuel_capacity : float
+
+@export_category("Structural Information")
+
+@export
+var health_points : float
+
+@export
+var integrity : float
+
+var current_fuel : float
+var wear : float
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
