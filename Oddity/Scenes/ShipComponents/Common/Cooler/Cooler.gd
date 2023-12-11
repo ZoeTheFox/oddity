c33b129d9ed362d960179048688cdec89c1befb6
Zoe <cmdrprotogen27@gmail.com>
2023-12-11 14:08:04 +0100

    created cooler

---
+++ Oddity/Scenes/ShipComponents/Common/Cooler/Cooler.gd
@@ -0,0 +1,45 @@
+extends Node3D
+
+@export_category("Manufacturer Information")
+
+@export
+var manufacturer : String
+
+@export
+var model : String
+
+@export_category("Component Information")
+
+@export_range(0, 10, 1)
+var size_class : int
+
+@export
+var max_cooling_output : float
+
+@export
+var heat_per_cool : float
+
+@export
+var power_per_cool : float
+
+@export_category("Structural Information")
+
+@export
+var health_points : float
+
+@export
+var integrity : float
+
+var current_cooling_output : float
+var current_heat : float
+var wear : float
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
