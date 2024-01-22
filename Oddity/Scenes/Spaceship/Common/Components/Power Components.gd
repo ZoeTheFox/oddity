f6e894b7dde8151ed509de03ad74564639c43a07
Zoe <cmdrprotogen27@gmail.com>
2024-01-22 08:08:13 +0100

    power stuff is basically finished now

--- Oddity/Scenes/Spaceship/Common/Components/Power Components.gd
+++ Oddity/Scenes/Spaceship/Common/Components/Power Components.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 var component_list : Array
 
+signal powerplant_ui_signal(percentage : float)
+signal battery_ui_signal(percantage : float)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +15,10 @@ func _process(delta):
 	print("Power Usage")
 	print("Powerplant: " + str($"Power Plants".current_power_output) + " / " + str($"Power Plants".total_power_output))
 	print("Battery: " + str($Battery.total_stored_power) + " / " + str($Battery.total_power_storage))
+	
+	powerplant_ui_signal.emit($"Power Plants".current_power_output / $"Power Plants".total_power_output * 100)
+	battery_ui_signal.emit($Battery.total_stored_power / $Battery.total_power_storage * 100)
+
 
 func request_power(component : Node3D):	
 	var powerplant_power = $"Power Plants".request_power(component.required_power)
